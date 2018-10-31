fc3c55e4ec2e91f1de6bf58ccf9b1f8ddedb7b04
xiajiqiu1990@163.com <xiajiqiu1990@163.com>
2018-10-31 14:41:53 +0800

    Eureka域名注册优化

--- yyspring-cloud-project/yyspring-cloud-autoconfigure/src/main/java/com/duowan/yyspringcloud/autoconfigure/eureka/YyEurekaAutoConfiguration.java
+++ yyspring-cloud-project/yyspring-cloud-autoconfigure/src/main/java/com/duowan/yyspringcloud/autoconfigure/eureka/YyEurekaAutoConfiguration.java
@@ -1,12 +1,14 @@
 package com.duowan.yyspringcloud.autoconfigure.eureka;
 
 import com.netflix.appinfo.EurekaInstanceConfig;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 /**
  * @author Arvin
@@ -23,8 +25,25 @@ public class YyEurekaAutoConfiguration {
     public static class YyEurekaClientAutoConfiguration {
 
         @Bean
-        public EurekaClientInstanceConfigBeanPostProcessor eurekaClientInstanceConfigBeanPostProcessor(EurekaClientProperties properties) {
-            return new EurekaClientInstanceConfigBeanPostProcessor(80, properties.getDomain());
+        public EurekaClientInstanceConfigBeanPostProcessor eurekaClientInstanceConfigBeanPostProcessor(Environment environment, EurekaClientProperties properties) {
+
+            String domain = resolveDomain(environment, properties);
+
+            return new EurekaClientInstanceConfigBeanPostProcessor(80, domain);
+        }
+
+        private String resolveDomain(Environment environment, EurekaClientProperties properties) {
+            if (StringUtils.isNotBlank(properties.getDomain())) {
+                return properties.getDomain();
+            }
+
+            String domain = environment.getProperty("eureka.instance.hostname");
+            if (StringUtils.isBlank(domain)) {
+                throw new IllegalArgumentException("注册域名未知，请设置： yyspring.cloud.eureka.client.domain");
+            }
+
+            return domain;
+
         }
 
     }
